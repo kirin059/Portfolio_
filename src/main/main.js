a7f60b28cb9be45227f05834ae557b08dfa9e785
saebom <boma91@gmail.com>
2021-04-13 02:11:26 +0900

    Add: js 수정

--- src/main/main.js
+++ src/main/main.js
@@ -5,22 +5,48 @@ const nav = document.getElementById('nav')
 toggle.addEventListener('click', () => nav.classList.toggle('active'))
 
 // auto-text
-const textEl = document.getElementById('text')
-const text = 'I always ❤ challenging new things !'
 
-let idx = 1;
+// const textEl = document.getElementById('text')
+// const text = 'I always ❤ challenging new things !'
+
+// let idx = 1;
+
+// function writeText() {
+//     textEl.innerText = text.slice(0, idx);
+
+//     idx++
+
+
+//     if (idx > text.length) {
+//         idx = 1
+//     }
+
+//     setTimeout(writeText, 200)
+// }
+// writeText()
+
+
+const textEl = document.getElementById('text')
+const text = 'I always challenging new things !'
+const heart = '&nbsp<i class="fas fa-heart" style="color:red"></i>&nbsp';
+
+let idx = 0;
+let container = '';
+
+writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx);
-
-    idx++
-
-
-    if (idx > text.length) {
-        idx = 1
+    if (idx === text.length) {
+        container = '';
+        idx = 0;
+    } else if (idx === 8) {
+        container += heart;
+        idx++;
+    } else {
+        container += text[idx];
+        idx++;
     }
+    textEl.innerHTML = container;
 
-
-    setTimeout(writeText, 200)
+    setTimeout(writeText, 200);
 }
-writeText()
