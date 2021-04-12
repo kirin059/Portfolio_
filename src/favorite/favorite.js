a99275261e40dffd82e86e2d33018d8552c427b6
saebom <boma91@gmail.com>
2021-04-13 00:02:43 +0900

    Add: favorite 기능 추가

--- src/favorite/favorite.js
+++ src/favorite/favorite.js
@@ -1,42 +1,37 @@
 'use strict';
 
-const body = document.body
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length;
+console.log(slidesLength)  // right slide 4개
 
-let activeSlide = 0
+let activeSlideIndex = 0
 
-rightBtn.addEventListener('click', () => {
-    activeSlide++
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+console.log(slideLeft.style.top)  // -300vh
 
-    if (activeSlide > slides.length - 1) {
-        activeSlide = 0
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    console.log(sliderHeight)
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
     }
 
-    setBgToBody()
-    setActiveSlide()
-})
-
-leftBtn.addEventListener('click', () => {
-    activeSlide--
-
-    if (activeSlide < 0) {
-        activeSlide = slides.length - 1
-    }
-
-    setBgToBody()
-    setActiveSlide()
-})
-
-setBgToBody()
-
-function setBgToBody() {
-    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
-function setActiveSlide() {
-    slides.forEach((slide) => slide.classList.remove('active'))
-
-    slides[activeSlide].classList.add('active')
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    // y축으로 00px 움직임 (두번째 슬라이드: 2 * 슬라이드 높이)
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
